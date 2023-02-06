e96d50f03760cd160630640dc8c56cf38a4161a5
Malcolm-G <malcolm.githuka@gmail.com>
2023-02-06 11:28:19 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
+puts "Hello World!"
+print "Pass this test, please."
 # p [1,2,3]
 
 # def add
@@ -21,3 +21,5 @@ def add num1, num2
 end
 
 puts add(1,3) + add(2,2)
+
+p [1,2,3]
